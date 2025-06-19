eeef14cdcce4096653a7a6b4332108ce224f548f
Amritesh Anand <amriteshanand7@gmail.com>
2025-06-19 18:09:18 -0400

    added requirements.txt

--- LangChain/tools_agents.py
+++ LangChain/tools_agents.py
@@ -1,6 +1,19 @@
 from langchain_core.tools import tool
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain import hub
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+
+    model="gemini-2.0-flash-exp",
+    google_api_key=os.getenv("GEMINI_API_KEY"),
+    temperature=0.7,
+    max_output_tokens=1024
+)
 
 # Create tools using @tool decorator
 @tool
